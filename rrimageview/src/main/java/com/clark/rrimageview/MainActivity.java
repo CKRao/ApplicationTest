5fea9254b0ab1008f52e9ce23c85829363fb018d
ClarkRao <490089610@qq.com>
2016-11-22 22:49:26 +0800

    commit RRImageview

---
+++ rrimageview/src/main/java/com/clark/rrimageview/MainActivity.java
@@ -0,0 +1,66 @@
+package com.clark.rrimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private RRImageview gado;
+    private Button bt1, bt2, bt3, bt4, bt5, bt6, bt7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        gado = (RRImageview) findViewById(R.id.gado);
+        bt1 = (Button) findViewById(R.id.bt1);
+        bt2 = (Button) findViewById(R.id.bt2);
+        bt3 = (Button) findViewById(R.id.bt3);
+        bt4 = (Button) findViewById(R.id.bt4);
+        bt5 = (Button) findViewById(R.id.bt5);
+        bt6 = (Button) findViewById(R.id.bt6);
+        bt7 = (Button) findViewById(R.id.bt7);
+
+        bt1.setOnClickListener(this);
+        bt2.setOnClickListener(this);
+        bt3.setOnClickListener(this);
+        bt4.setOnClickListener(this);
+        bt5.setOnClickListener(this);
+        bt6.setOnClickListener(this);
+        bt7.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt1:
+                gado.setImageResource(R.drawable.g1);
+                break;
+            case R.id.bt2:
+                gado.setImageResource(R.drawable.g2);
+                break;
+            case R.id.bt3:
+                gado.setImageResource(R.drawable.g3);
+                break;
+            case R.id.bt4:
+                gado.setImageResource(R.drawable.g5);
+                break;
+            case R.id.bt5:
+                gado.setType(RRImageview.TYPE_ROUND);
+                break;
+            case R.id.bt6:
+                gado.setType(RRImageview.TYPE_CIRCLE);
+                break;
+            case R.id.bt7:
+                gado.setBorderRadius(90);
+                break;
+        }
+    }
+}
